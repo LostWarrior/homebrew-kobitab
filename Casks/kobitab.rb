9786d699555f2544f45b3b0383170e54206dcb54
Kirti <kirti.sbhardwaj@gmail.com>
2026-08-08 16:44:48 +0100

    cask: publish KobiTab v2.1.0

--- Casks/kobitab.rb
+++ Casks/kobitab.rb
@@ -1,6 +1,6 @@
 cask "kobitab" do
-  version "2.0.1"
-  sha256 "4a1f1563a62c949c93d2cb7aa6c1528d708ef6bd7912d3fd30bb01cea661b4a1"
+  version "2.1.0"
+  sha256 "4fe14ce5a91e4cac5367e6f7f06be93c609a12405ac0d06048ae14f93888f064"
 
   url "https://downloads.kobitab.com/releases/v#{version}/KobiTab-#{version}-mac-universal.dmg",
       verified: "downloads.kobitab.com/releases/"
